23a37842dc6d94b62f8f08d38169757240ea9496
Erik Burton <erikburton1@gmail.com>
2023-09-21 20:16:15 -0400

    Added more but struggling to get arrays to display

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,7 +3,7 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-let fullName = 'Erik Burton';
+const fullName = 'Erik Burton';
 let currentYear = new Date().getFullYear();
 const profilePicture = 'images/erikb_profile.jpeg';
 
@@ -12,25 +12,26 @@ const profilePicture = 'images/erikb_profile.jpeg';
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-var imageElement = document.querySelector('#profilePicture');
+
+var imageElement = document.querySelector('img');
 
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
+
 yearElement.textContent = currentYear;
+
 imageElement.setAttribute('src', profilePicture);
+
 imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 
 /* Step 5 - Array */
-var favoriteFoods = ['Sushi', 'Steak', 'Salmon', 'Salad', 'Yellow Curry', 'Burritos'];
-var newFavoriteFood = 'Tacos';
-favoriteFoods.push(newFavoriteFood);
-document.getElementById('food').textContent = favoriteFoods.join('<br>');
-favoriteFoods.shift();
-document.getElementById('food').textContent = favoriteFoods.join('<br>');
-favoriteFoods.pop();
-document.getElementById('food').textContent = favoriteFoods.join('<br>');
+
+let foodArray = ["Pizza", " Sushi", " Hot Dogs", " Steak", " Chicken"];
+
+
+
 
 
 
