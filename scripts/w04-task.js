3d64b892d9bc5d6f3b69c29ac8b3c2a0245065b9
Erik Burton <erikburton1@gmail.com>
2023-10-03 17:44:43 -0400

    Fist commit W04 Programming task

---
+++ scripts/w04-task.js
@@ -0,0 +1,53 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Erik Burton",
+    photo: 'images/erikb_profile.jpeg',
+    favoriteFoods: [
+        'Steak',
+        'Shrimp',
+        'Burritos',
+        'Spaghetti',
+        'Sushi'
+        ],
+    hobbies: [
+        'drums',
+        'muscle cars',
+        'programming'
+    ],
+
+    placesLived: [
+        
+    ]
+
+};
+
+
+
+/* Populate Profile Object with placesLive objects */
+
+myProfile.placesLived.push(
+    {
+        place: 'Colton, CA',
+        length: '40 years'
+    }
+);
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
