581661e330d86f176d3a9eb108d43e300aa5965a
Erik Burton <erikburton1@gmail.com>
2023-10-05 21:01:40 -0400

    Complete W04 Learning Activity

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -12,9 +12,9 @@ let myProfile = {
         'Sushi'
         ],
     hobbies: [
-        'drums',
-        'muscle cars',
-        'programming'
+        'Drums',
+        'Muscle Cars',
+        'Programming'
     ],
 
     placesLived: [
@@ -29,25 +29,60 @@ let myProfile = {
 
 myProfile.placesLived.push(
     {
-        place: 'Colton, CA',
-        length: '40 years'
-    }
+        place: 'Colton, California',
+        length: '40 years',
+    },
+    {
+        place: 'St. George, Utah',
+        length: '4 years',
+    },
+    {
+        place: 'St. Augustine, Florida',
+        length: '4 years'
+    },
+
 );
 
 
 /* DOM Manipulation - Output */
 
+
 /* Name */
+document.querySelector('#name').textContent = myProfile.name;
 
 /* Photo with attributes */
-
+let photoElement = document.querySelector("#photo");
+photoElement.src = myProfile.photo;
+photoElement.alt = myProfile.name;
 
 /* Favorite Foods List*/
+myProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement("li");
+    li.textContent = food;
+    document.querySelector("#favorite-foods").appendChild(li);
+});
 
 
 /* Hobbies List */
+myProfile.hobbies.forEach(hobby => {
+    let li = document.createElement("li");
+    li.textContent = hobby;
+    document.querySelector("#hobbies").appendChild(li);
+});
+
 
 
 /* Places Lived DataList */
+myProfile.placesLived.forEach(place => {
+    let dt = document.createElement("dt");
+    dt.textContent = place.place;
+
+    let dd = document.createElement("dd");
+    dd.textContent = place.length;
+
+    document.querySelector("#places-lived").appendChild(dt);
+    document.querySelector("#places-lived").appendChild(dd);
+});
+
 
 
