9ba2a74dbfdd838813e619b911c41eea34642c89
Erik Burton <erikburton1@gmail.com>
2023-10-14 12:58:13 -0400

    async displayTemples Function

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -4,8 +4,29 @@
 
 const templesElement = document.getElementById('');
 
+const templelist = [];
+
 
 /* async displayTemples Function */
+
+const displayTemples = async (temples) => {
+    templesElement.innerHTML = ''; // Clear exisiting content
+
+    temples.forEach((temple) => {
+        const article = document.createElement('article');
+        const h3 = document.createElement('h3');
+        h3.textContent = temple.templeName;
+
+        const img = document.createElement('img');
+        img.src = temple.imageUrl;
+        img.alt = temple.location;
+
+        article.appendChild(h3);
+        article.appendChild(img);
+
+        templesElement.appendChild(article);
+    });
+};
 
 
 
