50f3db6885e21d506d619713c00b3bdc0b462291
Erik Burton <erikburton1@gmail.com>
2023-10-14 12:48:00 -0400

    Declare global variables

---
+++ scripts/w05-task.js
@@ -0,0 +1,25 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+
+const templesElement = document.getElementById('');
+
+
+/* async displayTemples Function */
+
+
+
+
+/* async getTemples Function using fetch()*/
+
+
+/* reset Function */
+
+
+/* sortBy Function */
+
+
+
+getTemples();
+
+/* Event Listener */
