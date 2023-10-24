3c54ef2ddd677cc61b09a2afed619b8113b9aeb1
Erik Burton <erikburton1@gmail.com>
2023-10-23 21:27:54 -0400

    Completes weatherModule.js

--- scripts/weatherModule.js
+++ scripts/weatherModule.js
@@ -2,7 +2,23 @@
 
 export const fetchWeather = async (zipCode) => {
     // Defines API key for the OpenWeatherMap API
-    const API_KEY = "ba8471e44eecd63a6b38dd887c2cfe54";
-  
+    const apiKey = "ba8471e44eecd63a6b38dd887c2cfe54";
+    // Use the fetch function to make an asychonous call to the OpenWeatherMap API
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}&units=imperial`);
+
+    // Check if the response from the API is not succussful (zip code not found)
+    if (!response.ok) {
+        throw new Error("Zip Code not found");
     }
-}
+
+    // Parse the JSON data received from the API call
+    const data = await response.json();
+
+    // Extract weather data from the API call
+    const city = data.name; 
+    const temerature = Math.round(data.main.temp);
+    const weatherDescription = data.weather[0].description;
+
+    // Return an object with the extracted weather information
+    return { city, temerature, weatherDescription};
+};
