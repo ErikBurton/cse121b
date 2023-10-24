118bca6550d86ec93be2c8a464731859881481a5
Erik Burton <erikburton1@gmail.com>
2023-10-23 21:42:00 -0400

    Adds lots of functionality to main_final.js

--- scripts/main_final.js
+++ scripts/main_final.js
@@ -9,4 +9,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const temperature = document.getElementById("temperature"); // Get the element to display the temperature
     const description = document.getElementById("description"); // Get the element to display the weather description
 
-})
+// Add a click event listener to the "Get Weather" button
+getWeatherButton.addEventListener("click", async () => {
+    const zipCode = zipCodeInput.value;
+
+    try {
+        // Attempt to fetch weather for the zip provieded by the user
+        const data = await fetchWeather(zipCode);
+
+        // Display the extracted data on the page
+        cityName.textContent = `City: ${data.city}`;
+        temperature.textContent = `Temperature: ${data.temerature}`;
+        description.textContent = `Description: ${data.weatherDescription}`
+    } catch (error) {
+        // If an error occurs fetching the data, show an error message
+        cityName.textContent = ""; // Clear the city name
+        temperature.textContent = `Error: ${error.message}`; // Display error message
+        description.textContent = ""; // Clear the weather description        
+    }
+    });   
+});
