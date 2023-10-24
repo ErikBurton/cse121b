040bded4af156874db2ef9e2530c85e7932d7d2c
Erik Burton <erikburton1@gmail.com>
2023-10-23 21:20:00 -0400

    Creates new weatherModule.js

---
+++ scripts/weatherModule.js
@@ -0,0 +1,8 @@
+// Module for fetching weather data
+
+export const fetchWeather = async (zipCode) => {
+    // Defines API key for the OpenWeatherMap API
+    const API_KEY = "ba8471e44eecd63a6b38dd887c2cfe54";
+  
+    }
+}
