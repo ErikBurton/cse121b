b8fe97d628c2699af76e85c03ebefa574b3152dc
Erik Burton <erikburton1@gmail.com>
2023-10-27 16:29:38 -0400

    Fixed some typo's

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -2,7 +2,7 @@
 
 /* Declare and initialize global variables */
 const templesElement = document.getElementById('temples');
-const templelist = [];
+let templelist = [];
 
 /* async displayTemples Function */
 const displayTemples = async (temples) => {
@@ -54,7 +54,7 @@ const sortBy = (temples) => {
             displayTemples(temples.filter(temple => temple.location.includes("Utah")));
             break;
         case 'older':
-            displayTemples(temples.filter(temple => new Date(temeple.dedicatedDate) < new Date(1950, 0, 1)));
+            displayTemples(temples.filter(temple => new Date(temple.dedicatedDate) < new Date(1950, 0, 1)));
             break;
             default:
                 displayTemples(temples);
